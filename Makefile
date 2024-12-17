c0e1a805d96532d760a1caf59e26bc5a1897feb4
Jan Doskočil <30779172+Dook97@users.noreply.github.com>
2024-12-17 12:14:29 +0100

    refactoring

--- Makefile
+++ Makefile
@@ -1,14 +1,14 @@
 .PHONY: all clean
 
 # use 'make EXTERNFLAGS=...' to specify own compiler flags
-# in particular 'make EXTERNFLAGS="-DDEBUG -DCOLOR -g"' to make a debug build
+# in particular 'make EXTERNFLAGS="-DCOLOR -g"' to make a debug build
 
 CC = cc
 CFLAGS = -std=c99 -D_XOPEN_SOURCE=700 -Wall -Wextra -Wpedantic -I./include
 LDFLAGS =
 
-SOURCES = src/main.c src/mqtt.c src/streambuf.c
-HEADERS = include/magic.h include/main.h include/mqtt.h include/vector.h include/streambuf.h
+SOURCES ::= $(wildcard src/*.c)
+HEADERS ::= $(wildcard include/*.h)
 
 all: mqttserver
 
--- src/streambuf.c
+++ src/streambuf.c
@@ -1,21 +1,21 @@
-#include <stdlib.h>
 #include <assert.h>
+#include <stdlib.h>
 #include <string.h>
 
+#include "magic.h"
 #include "streambuf.h"
 
 #define STREAMBUF_DEFAULT_SIZE 2048
 
 streambuf *sbuf_make(void) {
-	streambuf *ret = malloc(sizeof(streambuf) + STREAMBUF_DEFAULT_SIZE);
-	if (ret == NULL)
-		return NULL;
+	streambuf *ret = xmalloc(sizeof(streambuf) + STREAMBUF_DEFAULT_SIZE);
 
 	*ret = (streambuf){
 		.begin = 0,
 		.end = 0,
 		.cap = STREAMBUF_DEFAULT_SIZE,
 	};
+
 	return ret;
 }
 
@@ -42,11 +42,11 @@ streambuf *sbuf_make_fit(streambuf *sb, size_t size) {
 
 	/* if that doesn't cut it, realloc */
 	if (size > SB_AVAIL(sb)) {
-		size_t new_size = STREAMBUF_DEFAULT_SIZE;
+		size_t new_size = sb->cap;
 		while (new_size < size)
 			new_size *= 2;
 		sb->cap = new_size;
-		sb = realloc(sb, sizeof(streambuf) + new_size);
+		sb = xrealloc(sb, sizeof(streambuf) + new_size);
 	}
 
 	assert(sb->end <= sb->cap);
@@ -61,8 +61,6 @@ ssize_t sbuf_load(streambuf **sbuf, int fd, size_t count) {
 	assert(sb->end <= sb->cap);
 
 	sb = sbuf_make_fit(sb, SB_DATASIZE(sb) + count);
-	if (sb == NULL)
-		return SBUF_ENOMEM;
 
 	ssize_t nread = read(fd, &sb->data[sb->end], count);
 	if (nread > 0)
@@ -75,11 +73,11 @@ ssize_t sbuf_load(streambuf **sbuf, int fd, size_t count) {
 	return nread;
 }
 
-void sbuf_mark_read(streambuf *sbuf, size_t count) {
+void sbuf_mark_read(streambuf *sbuf, size_t nread) {
 	assert(sbuf->begin <= sbuf->end);
 	assert(sbuf->end <= sbuf->cap);
 
-	sbuf->begin += count;
+	sbuf->begin += nread;
 	if (sbuf->begin == sbuf->end)
 		sbuf->begin = sbuf->end = 0;
 
